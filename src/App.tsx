e2fd96ecf55987eb05809692072c977f18e738e0
artemsynytsia <synartem2023@gmail.com>
2026-01-20 19:22:29 +0200

    style: basic styles in src/index.css and changing styles in ui-componenrs/input/Input.module.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import './App.css';
 import { Input } from './ui-components/input';
 
+
 const App = () => {
 	return (
 		<div>
-			Home
 			<Input placeholder="Search" fullWidth />
 		</div>
 	);
--- src/ui-components/input/Input.tsx
+++ src/ui-components/input/Input.tsx
@@ -2,27 +2,31 @@
 import React from 'react';
 import styles from './Input.module.css';
 
+
 type Size = 'XL' | 'big' | 'medium' | 'small' | 'XS';
 type Theme = 'primary' | 'secondary' | 'success' | 'error';
 
 interface InputProps {
 	value?: string;
+	type?: string;
 	placeholder?: string;
 	fullWidth?: boolean;
+	disabled?: boolean;
 	size?: Size;
 	theme?: Theme;
 	onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const Input = ({ value, placeholder, fullWidth, size = 'medium', theme, onChange }: InputProps) => {
+const Input = ({ value, type, placeholder, fullWidth, disabled = false, size = 'medium', theme = 'primary', onChange }: InputProps) => {
 	let className = styles.input;
 
 	if (size) className += ' ' + styles[size];
 	if (theme) className += ' ' + styles[theme];
 	if (fullWidth) className += ' ' + styles.fullWidth;
+	if (disabled) className += ' ' + styles.disabled;
 
 	return (
-		<input value={value} placeholder={placeholder} className={className} onChange={onChange} />
+		<input value={value} type={type} placeholder={placeholder} className={className} onChange={onChange}/>
 	);
 };
 
