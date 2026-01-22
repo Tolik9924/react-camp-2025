fb1ef6b23dbbde0fb952192d3da57b6dadb2e62e
artemsynytsia <synartem2023@gmail.com>
2026-01-22 12:16:26 +0200

    refactor: label, objects size,themeClasses

--- src/ui-components/input/Input.tsx
+++ src/ui-components/input/Input.tsx
@@ -2,15 +2,24 @@ import React from 'react';
 import styles from './Input.module.css';
 import cn from 'classnames';
 
-
-
-
-
 type Size = 'XL' | 'big' | 'medium' | 'small' | 'XS';
 type Theme = 'primary' | 'secondary' | 'success' | 'error';
+const sizeClasses = {
+	XL: styles.XL,
+	big: styles.big,
+	medium: styles.medium,
+	small: styles.small,
+    XS: styles.XS,
+}
+
+const themeClasses = {
+	primary: styles.primary,
+	secondary: styles.secondary,
+	success: styles.success,
+	error: styles.error,
+}
 
 interface InputProps {
-
 	value?: string;
 	id?: string;
 	label?: string;
@@ -21,46 +30,46 @@ interface InputProps {
 	size?: Size;
 	theme?: Theme;
 	onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
-	
 }
 
-const Input = ({ value, id, label, type, placeholder, fullWidth, disabled = false, size = 'medium', theme = 'primary', onChange }: InputProps) => {
+const Input = ({
+	value,
+	id,
+	label,
+	type,
+	placeholder,
+	fullWidth,
+	disabled = false,
+	size = 'medium',
+	theme = 'primary',
+	onChange,
+}: InputProps) => {
+	const className = cn(
+		styles.input,
+		sizeClasses[size],
+		themeClasses[theme],
 
-   const className = cn(
-
-    styles.input,
-	styles[size],
-	styles[theme],
-  
-   {
-
-    [styles.fullWidth] : fullWidth,
-    [styles.disabled] : disabled
-
-    }
-    );
-
-	return (
-	
-	<div>
-        {
-			label &&(
-				<label htmlFor='id'> </label>			
-			)
-		}
-
-        <input
-	    value={value} 
-	    id={id} 
-		type={type} 
-	    placeholder={placeholder} 
-	    className={className} 
-	    onChange={onChange} 
-    	/>
-
-    </div>
+		{
+			[styles.fullWidth]: fullWidth,
+			[styles.disabled]: disabled,
+		},
 	);
 
+	return (
+		<div>
+			{label && id && <label htmlFor={id}>{label}</label>}
+
+
+			<input
+				value={value}
+				id={id}
+				type={type}
+				placeholder={placeholder}
+				className={className}
+				onChange={onChange}
+			/>
+		</div>
+	);
 };
 
 export default Input;
